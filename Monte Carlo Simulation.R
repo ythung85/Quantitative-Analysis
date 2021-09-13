6223af308dcc55a9d62bfa751388cd9a35b40da9
Hung, Yi-Ting <a080528@gmail.com>
2021-09-12 21:53:57 -0400

    initial commit

---
+++ Monte Carlo Simulation.R
@@ -0,0 +1,16 @@
+###############################################################################
+##                           HW 3 Answer Sheet         			    		         ##
+##            	        Name :洪易廷   NTU ID:R09522505  	    	     	    	 ##
+###############################################################################
+
+rm(list=ls(all=T))
+jpeg("MatrixPlot.jpeg",width=900,height=900)
+
+  # your code here........
+  # your code here........
+  # your code here........
+
+dev.off()
+
+Sample.size <- c(10,500)
+number.replication <- 1000
